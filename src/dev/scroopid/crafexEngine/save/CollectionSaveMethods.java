64a6bd7ffd3aeb780ee3113569069e3b2f07e503
James Womack <Conolev@gmail.com>
2014-01-09 17:02:25 -0800

    Added SaveManager for Collection Types, Need to finish implementation of Saving them...

--- src/dev/scroopid/crafexEngine/save/CollectionSaveMethods.java
+++ src/dev/scroopid/crafexEngine/save/CollectionSaveMethods.java
@@ -9,6 +9,7 @@ import java.util.List;
 import java.util.Map;
 
 import dev.scroopid.crafexEngine.Logger;
+import dev.scroopid.crafexEngine.save.util.SaveConstants;
 import dev.scroopid.crafexEngine.save.util.SaveException;
 import dev.scroopid.crafexEngine.save.util.SaveUtils;
 
@@ -55,21 +56,56 @@ public class CollectionSaveMethods {
 	public static String[] saveCollection(Object target, Field field, int callsDeep) {
 		List<String> data = new ArrayList<String>();
 		Object rawObject = SaveUtils.getData(target, field);
-
-		// Lets get some basic type info
-		Class<?> klass = rawObject.getClass();
 		
+		// Create Data Header
+		data.add(SaveUtils.formatCollection(field.getName(), field.getType().getName(), SaveUtils.getGenericType(field).getName()));
+		data.add(SaveConstants.DATA_START);
 		
+		// put null token if the object is NULL
+		if (rawObject == null){
+			data.add(SaveUtils.tabify(SaveConstants.NULL, callsDeep));
+			data.add(SaveConstants.DATA_END);
+			return (String[])data.toArray();
+		}
 		
-		// Create collection header
-		
-		
-		return (String[])data.toArray();
+		return (String[])walkCollection((Collection<?>)rawObject, field,callsDeep).toArray();
 	}
 	
-	private static List<String> walkCollection(Collection<?> collection, int callsDeep){
+	private static List<String> walkCollection(Collection<?> collection, Field field, int callsDeep){
 		List<String> data = new ArrayList<String>();
 		
+		LOGGER.info(String.format("Walking %s field when we are %d calls deep", field.getName(), callsDeep));
+		
+		for (Object obj : collection){
+			if (obj == null){
+				// Data is null, Dont Care
+				data.add(SaveConstants.NULL);
+				LOGGER.debug("Saved NULL Variable");
+			} else{
+				if (SaveUtils.canCastTo(obj.getClass(), Collection.class)){
+					// This is a Collection Within a Collection, lets walk this!
+					try{
+						// Try and get the innerGenericType and create the collection header
+						Class<?> innerGenericType = SaveUtils.getGenericType((Collection<?>)obj);
+						
+						// Walk the inner Collection!
+						data.add(SaveUtils.tabify(SaveUtils.formatCollection(field.getName(), obj.getClass().getName(), innerGenericType.getName()), callsDeep));
+						data.add(SaveUtils.tabify(SaveConstants.DATA_START, callsDeep));
+						data.addAll(walkCollection((Collection<?>)obj, field, callsDeep + 1));
+						data.add(SaveConstants.DATA_END);
+						
+					} catch(SaveException e){
+						LOGGER.debug("Unable to get the generic type of the inner Collection, saving it as null.");
+						data.add(SaveConstants.NULL);
+					}
+				} else if (SaveUtils.canCastTo(obj.getClass(), Map.class)){
+					// This is a Map within a Collection, lets Walk this!
+					
+				} else {
+					
+				}
+			}
+		}
 		
 		return data;
 	}
